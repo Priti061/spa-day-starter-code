164e4d3684ffe3a2f63ba2876342320b8258f50b
Priti061 <srijanabaral94@gmail.com>
2023-10-10 14:47:01 -0500

    Added validation code

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,19 +1,35 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.*;
+
 public class User {
+    @NotBlank
+    @NotNull
+    @Size(min = 5, max = 15, message="Username must be 5-15 characters long.")
     private String username;
+
+    @Email(message= "Invalid email format.")
     private String email;
+
+    @NotBlank
+    @NotNull
+    @Size(min = 6, message= "Password must be at least 6 characters long.")
     private String password;
+
+    // BONUS MISSION
+    @NotNull(message= "Passwords do no match.")
+    private String verifyPassword;
 
     public User() {
 
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, String verifyPassword) {
         this();
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verifyPassword= verifyPassword;
     }
 
     public String getUsername() {
@@ -38,5 +54,21 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword();
+    }
+    // BONUS MISSION
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
+    }
+
+    public void checkPassword() {
+        if (password != null && verifyPassword !=null && !password.equals(verifyPassword)) {
+            setVerifyPassword(null);
+        }
     }
 }
